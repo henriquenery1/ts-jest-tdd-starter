d4322e8b9cc196fe360442b0db20d6f6235da81d
Henrique Nery <henriquenery07@gmail.com>
2022-08-11 10:37:34 -0300

    feat: add method multiplication

--- src/calc.ts
+++ src/calc.ts
@@ -2,6 +2,9 @@ class Calc {
   add(a: number, b: number): number {
     return a + b;
   }
-}
 
+  multiplication(a: number, b: number): number {
+    return a * b
+  }
+}
 export default new Calc();
